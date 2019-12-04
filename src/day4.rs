50d9ab55f51dbc8d1ed34fb0d0104f927bcdbb06
John Ledbetter <john@callrail.com>
2019-12-04 09:57:42 -0500

    Day4 part2

--- src/day4.rs
+++ src/day4.rs
@@ -1,29 +1,55 @@
 const INPUT : std::ops::Range<usize> = 265275..781584;
 
-pub fn is_valid_password(v: usize) -> bool {
-    let digits = vec![
+pub fn digits_are_increasing(digits: &Vec<usize>) -> bool {
+    digits
+        .iter()
+        .enumerate()
+        .skip(1)
+        .all(|(index, &digit)| digit >= digits[index - 1])
+}
+
+pub fn digits_contain_repeated_value(digits: &Vec<usize>) -> bool {
+    digits
+        .iter()
+        .enumerate()
+        .skip(1)
+        .any(|(index, &digit)| digit == digits[index - 1])
+}
+
+pub fn digits_contain_repeated_value_exactly_once(digits: &Vec<usize>) -> bool {
+    digits
+        .iter()
+        .fold(vec![0; 10], |mut acc, &d|{
+            acc[d] += 1;
+            acc
+        })
+        .iter()
+        .any(|&count| count == 2)
+}
+
+pub fn digits_for(v: usize) -> Vec<usize> {
+    vec![
         v / 100_000,
         v / 10_000 % 10,
         v / 1_000  % 10,
         v / 100    % 10,
         v / 10     % 10,
         v / 1      % 10
-    ];
-
-    let is_increasing = digits
-        .iter()
-        .enumerate()
-        .skip(1)
-        .all(|(index, &digit)| digit >= digits[index - 1]);
-
-    let has_one_dupe = digits
-        .iter()
-        .enumerate()
-        .skip(1)
-        .any(|(index, &digit)| digit == digits[index - 1]);
-
-    is_increasing && has_one_dupe
+    ]
 }
+
+pub fn is_valid_password(v: usize) -> bool {
+    let digits = digits_for(v);
+    digits_are_increasing(&digits) && digits_contain_repeated_value(&digits)
+}
+
+pub fn is_valid_password_p2(v: usize) -> bool {
+    let digits = digits_for(v);
+
+    digits_are_increasing(&digits) &&
+        digits_contain_repeated_value_exactly_once(&digits)
+}
+
 
 
 #[cfg(test)]
@@ -44,7 +70,14 @@ mod tests {
             .count();
 
         assert_eq!(c, 960);
-
     }
 
+    #[test]
+    fn p2_solution() {
+        let c = INPUT
+            .filter(|&v| is_valid_password_p2(v))
+            .count();
+
+        assert_eq!(c, 626);
+    }
 }
