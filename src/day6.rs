422df2250090d1bddfc621df06c6bcf68a53e024
John Ledbetter <john@callrail.com>
2019-12-06 10:38:44 -0500

    Clean up solution & implement ancestors as an Iterator

--- src/day6.rs
+++ src/day6.rs
@@ -2,34 +2,75 @@ use std::collections::HashMap;
 
 const INPUT : &'static str = include_str!("../inputs/day6.txt");
 
+#[derive(Debug)]
 struct OrbitMap {
     map: HashMap::<String, String>
 }
 
-#[derive(Debug)]
-enum MapLoadError {
-    ParsError
+// An interator which yields the ancestors of the given object.
+// For example, given A -- B -- C -- D, ancestors(D) will yield C, B, A.
+struct OrbitMapAncestorIterator<'a> {
+    current:   &'a str,
+    orbit_map: &'a OrbitMap
 }
 
+impl<'a> Iterator for OrbitMapAncestorIterator<'a> {
+    type Item = &'a str;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.orbit_map
+            .get(self.current)
+            .map(|parent|{
+                self.current = parent;
+                parent.as_str()
+            })
+    }
+}
+
+#[derive(Debug,PartialEq)]
+enum MapLoadError {
+    ParseError(String)
+}
 
 impl OrbitMap {
+    // Given a string containing a single specification "A)B" per line,
+    // Return an OrbitMap representing the relationship between all the
+    // contents.
+    // Returns a MapLoadError if something goes wrong.
+    pub fn parse(input: &str) -> Result<OrbitMap, MapLoadError> {
+        // a list of (key, value) tuples can be collected into a HashMap.
+        let map = input
+            .lines()
+            .map(OrbitMap::parse_line)
+            .collect::<Result<HashMap<String, String>, MapLoadError>>()?;
+
+        Ok(OrbitMap { map: map })
+    }
+
+    // Given a line like "YOU)SAN" return a tuple of ("YOU", "SAN").
+    // Returns an Error if the line has too many or not enough parts.
+    fn parse_line(line: &str) -> Result<(String, String), MapLoadError> {
+        let mut parts = line.split(")");
+        // ensure that the line is exactly two items separated by a )
+        match (parts.next(), parts.next(), parts.next()) {
+            (Some(a), Some(b), None) => Ok((b.to_string(), a.to_string())),
+            _ => Err(MapLoadError::ParseError(line.to_string()))
+        }
+    }
+
     pub fn len(&self) -> usize {
         self.map.len()
     }
 
-    pub fn parse(input: &str) -> Result<OrbitMap, MapLoadError> {
-        let map = input
-            .lines()
-            .map(|line|{
-                let mut parts = line.split(")");
-                let orbitee = parts.next().ok_or(MapLoadError::ParsError)?;
-                let orbiter = parts.last().ok_or(MapLoadError::ParsError)?;
+    // Given the name of an object, like 'SAN', return the parent object,
+    // That is the object that the given key orbits.
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.map.get(key)
+    }
 
-                Ok((orbiter.to_string(), orbitee.to_string()))
-            })
-            .collect::<Result<HashMap<String, String>, MapLoadError>>()?;
-
-        Ok(OrbitMap { map: map })
+    // Returns an interator that yields all the parents of the given object.
+    pub fn ancestors<'a>(&'a self, object: &'a str) -> OrbitMapAncestorIterator<'a> {
+        OrbitMapAncestorIterator { current: object, orbit_map: self }
     }
 
     pub fn total_orbit_count(&self) -> usize {
@@ -47,61 +88,41 @@ impl OrbitMap {
             .sum()
     }
 
+    // Returns how many nodes are above this node in the tree.
     fn orbit_count(&self, orbiter: &str) -> usize {
-        let mut count = 0;
-        let mut o = orbiter;
-
-        while let Some(orbitee) = &self.map.get(o) {
-            o = orbitee;
-            count += 1;
-        }
-
-        count
+        self
+            .ancestors(orbiter)
+            .count()
     }
 
-    pub fn parent_distance_from(&self, ancestor: &str, target: &str) -> Option<usize> {
-        let mut o = target;
-        let mut distance = 0;
-
-        loop {
-            if let Some(parent) = &self.map.get(o) {
-                if parent == &ancestor {
-                    break;
-                }
-                distance += 1;
-                o = parent;
-            } else {
-                return None;
-            }
-        }
-
-        println!("{} is {} from {}", ancestor, distance, target);
-        Some(distance)
+    // Return the distance from the parent node to the target node.
+    // Returns None if the ancestor is not actually an ancestor of target.
+    pub fn distance_from_parent(&self, parent: &str, target: &str) -> Option<usize> {
+        self
+            .ancestors(target)
+            .position(|ancestor| parent == ancestor)
     }
 
-    pub fn transfer_distance(&self, from: &str, to: &str) -> Option<usize> {
+    // Return the distance between two nodes, through their common parent node.
+    // If they do not have a common parent (they totally should?) returns None.
+    pub fn transfer_distance(&self, first: &str, second: &str) -> Option<usize> {
         // Goal is to find the lowest common ancenstor of `from` and `to`, and then sum
         // distance from the ancestor to each node.
+        let result = self
+            .ancestors(second)
+            .map(|ancestor| (ancestor, self.distance_from_parent(ancestor, first)) )
+            .find(|(_, dist)| dist.is_some());
 
-        let mut o = to;
 
-        while let Some(parent) = &self.map.get(o) {
-            if let Some(d) = self.parent_distance_from(parent, from) {
-
-                return Some(d + self.parent_distance_from(parent, to).unwrap());
-            }
-            o = parent;
-        }
-
-        None
+        result.and_then(|(parent, distance)|{
+            distance.and_then(|value| Some(value + self.distance_from_parent(parent, second).unwrap()))
+        })
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
 
     #[test]
     fn p1_example() {
@@ -135,5 +156,16 @@ mod tests {
 
         assert_eq!(m.transfer_distance("YOU", "SAN"), Some(373));
 
+    }
+
+    #[test]
+    fn parse_error_works() {
+        let m = OrbitMap::parse("COM)B)C");
+        assert_eq!(m.is_err(), true);
+        assert_eq!(m.unwrap_err(), MapLoadError::ParseError("COM)B)C".to_string()));
+
+        let m = OrbitMap::parse("A)B\nCOM");
+        assert_eq!(m.is_err(), true);
+        assert_eq!(m.unwrap_err(), MapLoadError::ParseError("COM".to_string()));
     }
 }
